17fd2b778a72ce91f7f520387eee6a9ee4a23223
pruthviprabhu <pruthviprabhus@gmail.com>
2023-06-17 11:08:55 +0530

    connect database

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(cookie());
 app.use(express.json());
+db.connect((err) => {
+    if(err)throw err;
+    console.log("database connected");
+})
+app.listen(PORT);
--- routes/dbconfig.js
+++ routes/dbconfig.js
@@ -1,8 +1,9 @@
 const sql = require("mysql");
+const dotenv = require("dotenv").config();
 const database = sql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DATABASE,
 })
-module.exports = db;
+module.exports = database;
