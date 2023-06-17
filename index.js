e921325830da14d2443ffa142a4400f075f4cca1
pruthviprabhu <pruthviprabhus@gmail.com>
2023-06-17 10:56:34 +0530

    add files dotenv and initialize index

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const db = require("./routes/dbconfig");
+const app = express();
+const cookie = require("cookie-parser");
+const PORT = process.env.PORT || 5000;
+app.use("/js", express.static(__dirname + "./public"));
+app.use("/css", express.static(__dirname + "./public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(cookie());
+app.use(express.json());
---
+++ routes/dbconfig.js
@@ -0,0 +1,8 @@
+const sql = require("mysql");
+const database = sql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+})
+module.exports = db;
